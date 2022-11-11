fad60d4f8bf10fb51ef90cd7898f3a0e03ee3ee5
Katariina83 <rikkonenkatariina@gmail.com>
2022-11-11 11:16:02 +0200

    Adding image slider

--- js/scripts.js
+++ js/scripts.js
@@ -6,3 +6,15 @@ toggleButton.addEventListener("click", popMenu);
 function popMenu() {
     navbarLinks.classList.toggle("active");
 }
+
+//Adding a timer to change the images automatically
+var counter = 1;
+    setInterval(function()  {
+      document.getElementById('radio'+ counter).checked = true;
+      counter++; 
+
+      if(counter>4){  //4 means it slides through all the 4 pictures
+        counter=1
+      }
+
+    }, 5000);
