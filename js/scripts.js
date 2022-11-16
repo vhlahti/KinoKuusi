14895d26724c73de0e3ce6dbfc01c5a8b1b7e992
Suvikan <sanledell@gmail.com>
2022-11-16 12:11:03 +0200

    add javascript for 'thisweek' counter

--- js/scripts.js
+++ js/scripts.js
@@ -18,3 +18,14 @@ var counter = 1;
       }
 
     }, 5000);
+
+//What week is it
+currentDate = new Date();
+startDate = new Date(currentDate.getFullYear(), 0, 1);
+var days = Math.floor((currentDate - startDate) /
+  (24 * 60 * 60 * 1000));
+  
+var weekNumber = Math.ceil(days / 7);
+var weekNro = document.createTextNode(weekNumber);
+var week = document.getElementById("thisweek");	
+week.appendChild(weekNro);
