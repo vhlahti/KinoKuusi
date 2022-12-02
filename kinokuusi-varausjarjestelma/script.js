96248e7668802c03366bd27681ebac900cedd849
Suvikan <sanledell@gmail.com>
2022-12-02 11:38:00 +0200

    edit varausjarjestelma to be responsive and add buttons
    edited varausjärjestelmä to flex and added 'varaa' 'osta and 'peruuta' buttons which have no function yet

--- kinokuusi-varausjarjestelma/script.js
+++ kinokuusi-varausjarjestelma/script.js
@@ -1,69 +1,73 @@
-const container = document.querySelector('.container');
-const seats = document.querySelectorAll('.row .seat:not(.occupied');
-const count = document.getElementById('count');
-const total = document.getElementById('total');
-const movieSelect = document.getElementById('movie');
+const container = document.querySelector(".salikartta");
+const seats = document.querySelectorAll(".row .seat:not(.sold)");
+const count = document.getElementById("count");
+const total = document.getElementById("total");
+const movieSelect = document.getElementById("movie");
 
-populateUI();
-let ticketPrice = +movieSelect.value;
+populateUI()
 
-// Save selected movie index and price
+let ticketPrice = +movieSelect.nodeValue;
+
 function setMovieData(movieIndex, moviePrice) {
-  localStorage.setItem('selectedMovieIndex', movieIndex);
-  localStorage.setItem('selectedMoviePrice', moviePrice);
+    localStorage.setItem("selectedMovieIndex", movieIndex);
+    localStorage.setItem("selectedmoviePrice", moviePrice);
 }
 
-// update total and count
-function updateSelectedCount() {
-  const selectedSeats = document.querySelectorAll('.row .seat.selected');
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
 
-  const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
 
-  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+    localStorage.setItem("selectedSeats", JSON.stringify(seatsIndex));
 
-  //copy selected seats into arr
-  // map through array
-  //return new array of indexes
+    const selectedSeatsCount = selectedSeats.length;
 
-  const selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
 
-  count.innerText = selectedSeatsCount;
-  total.innerText = selectedSeatsCount * ticketPrice;
+    setMovieData(movieSelect.selectedIndex, movieSelect.value);
 }
 
-// get data from localstorage and populate ui
 function populateUI() {
-  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
-  if (selectedSeats !== null && selectedSeats.length > 0) {
-    seats.forEach((seat, index) => {
-      if (selectedSeats.indexOf(index) > -1) {
-        seat.classList.add('selected');
-      }
-    });
-  }
+    const selectedSeats = JSON.parse(localStorage.getItem("selectedSeats"));
 
-  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+    if (selectedSeats !== null && selectedSeats.length > -1){
+        seats.forEach((seat, index) => {
+            if (selectedSeats.indexOf(index) > -1){
+                seat.classList.add("selected");
+            }
+        });
+    }
 
-  if (selectedMovieIndex !== null) {
-    movieSelect.selectedIndex = selectedMovieIndex;
-  }
+    const selectedMovieIndex = localStorage.getItem("selectedMovieIndex");
+
+    if(selectedMovieIndex !== null){
+        movieSelect.selectedIndex = selectedMovieIndex;
+    }
 }
 
-// Movie select event
-movieSelect.addEventListener('change', (e) => {
-  ticketPrice = +e.target.value;
-  setMovieData(e.target.selectedIndex, e.target.value);
-  updateSelectedCount();
-});
-
-// Seat click event
-container.addEventListener('click', (e) => {
-  if (e.target.classList.contains('seat') && !e.target.classList.contains('occupied')) {
-    e.target.classList.toggle('selected');
-
+movieSelect.addEventListener("change", (e) => {
+    ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
-  }
 });
 
-// intial count and total
-updateSelectedCount();
+container.addEventListener("click", (e) => {
+    if(
+        e.target.classList.contains("seat") &&
+        !e.target.classList.contains("sold")
+    ){
+        e.target.classList.toggle("selected");
+
+        updateSelectedCount();
+    }
+
+})
+
+
+//varausnappi teksti vaihtaa väriä (testi et script tekee jotain)
+
+function doSomething(){
+    const thanks = document.getElementById('btn-varaa');
+    thanks.classList.add('color');
+}
